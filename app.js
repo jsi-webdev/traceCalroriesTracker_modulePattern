1414ae64e0f3ad2971c189ff208bd1b29adfb50a
jsi-webdev <j.skullow@gmail.com>
2020-08-28 02:25:31 +0900

    implement public method : ItemCtrl - getItems / UICtrl - populateItemList

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const ItemCtrl = (function () {
 
   // Data Structure / State
   const data = {
-    item: [
+    items: [
       { id: 0, name: "Steak Dinner", calories: 1200 },
       { id: 1, name: "Cookies", calories: 400 },
       { id: 2, name: "Eggs", calories: 300 },
@@ -23,18 +23,44 @@ const ItemCtrl = (function () {
   };
 
   // Public Methods
-  return {};
+  return {
+    getItems: function () {
+      return data.items;
+    },
+  };
 })();
 // UI Controller
 const UICtrl = (function () {
+  const UISelectors = {
+    itemList: "#item-list",
+  };
   // Public Methods
-  return {};
+  return {
+    populateItemList: function (items) {
+      let html = "";
+
+      items.forEach((item) => {
+        html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name} : </strong> <em>${item.calories} calories</em>
+        <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+      </li>`;
+      });
+
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+  };
 })();
 // App Controller
 const App = (function (ItemCtrl, UICtrl) {
   // Public Methods
   return {
-    init: null,
+    init: function () {
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+      // Populate list with items
+      UICtrl.populateItemList(items);
+    },
   };
 })(ItemCtrl, UICtrl);
 
